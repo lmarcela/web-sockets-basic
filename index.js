afe09eaa45db40aa6b65eaf6343b681b1fb434d7
Marcela Malaver <marcela9409@gmail.com>
2023-03-02 16:07:53 -0500

    ✨ Enviar y recibir mensajes desde y hacia el servidor

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 app.use(express.static(__dirname + "/public"));
 io.on("connection", (socket) => {
-  console.log("cliente conectado", socket.id);
-  socket.emit('mensaje-bienvenida',{
-    msg: 'Bienvenido al server',
-    fecha: new Date()
-  })
+  //   console.log("cliente conectado", socket.id);
+  //   socket.emit("mensaje-bienvenida", {
+  //     msg: "Bienvenido al server",
+  //     fecha: new Date(),
+  //   });
+  //   socket.on("mensaje-cliente", (data) => {
+  //     console.log("el cliente envio algo", data);
+  //   });
+  socket.on("mensaje-al-servidor", (data) => {
+    console.log("el cliente envio algo", data);
+    io.emit('mensaje-del-servidor',data);
+  });
 });
 server.listen(8080, () => {
   console.log("server corriendo en puerto 8080!");
