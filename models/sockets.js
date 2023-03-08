1bf979cd2832c911e647cf9db9f0cefdd8ffc825
Marcela Malaver <marcela9409@gmail.com>
2023-03-08 11:37:32 -0500

    ✨ Actualizar marcadores en tiempo real

--- models/sockets.js
+++ models/sockets.js
@@ -11,13 +11,19 @@ class Sockets {
 
   socketEvents() {
     this.io.on("connection", (socket) => {
-      console.log("cliente conectado!");
+      // console.log("cliente conectado!");
+
       socket.emit("active-markers", this.markers.actives);
 
       socket.on("new-marker", (marker) => {
         this.markers.addMarker(marker);
-        //Para emitir a todos menos a quien lo creo
+        //socket.broadcast --> Para emitir a todos menos a quien lo creo
         socket.broadcast.emit("new-marker", marker);
+      });
+
+      socket.on("update-marker", (marker) => {
+        this.markers.updateMarker(marker);
+        socket.broadcast.emit("update-marker", marker);
       });
     });
   }
