d0bbb63ceea0103241f4254603bf857635f0ec2a
Marcela Malaver <marcela9409@gmail.com>
2023-03-09 09:21:33 -0500

    ✨ Crear usuario en BD Mongo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,33 @@
 const { response } = require("express");
+const User = require("../models/user");
 
 const createUser = async (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "new",
-  });
+  try {
+    const { email, password } = req.body;
+
+    const emailExist = await User.findOne({ email });
+    if (emailExist) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El correo ya existe",
+      });
+    }
+
+    const user = new User(req.body);
+
+    await user.save();
+
+    res.json({
+      ok: true,
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Contact administrator",
+    });
+  }
 };
 
 const login = async (req, res = response) => {
