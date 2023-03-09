abf0f81e55f51e1ad8e05ba260b1e3e4b96d6833
Marcela Malaver <marcela9409@gmail.com>
2023-03-09 09:03:07 -0500

    ✨ Middleware para validar campos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,4 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
 
 const createUser = async (req, res = response) => {
   res.json({
@@ -9,13 +8,6 @@ const createUser = async (req, res = response) => {
 };
 
 const login = async (req, res = response) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
   const { email, password } = req.body;
   res.json({
     ok: true,
--- router/auth.js
+++ router/auth.js
@@ -3,16 +3,27 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 const { createUser, login, renewToken } = require("../controllers/auth");
+const { validateFields } = require("../middlewares/validate-fields");
 
 const router = Router();
 
-router.post("/new", createUser);
+router.post(
+  "/new",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("password", "password is required").not().isEmpty(),
+    check("email", "email is required").isEmail(),
+    validateFields,
+  ],
+  createUser
+);
 
 router.post(
   "/",
   [
     check("email", "email is required").isEmail(),
     check("password", "password is required").not().isEmpty(),
+    validateFields,
   ],
   login
 );
